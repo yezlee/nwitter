aeb44326f6421085cb2501e9ce11a8072464ad8c
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-14 14:57:19 +0900

    React + Firebase setup using .env file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
