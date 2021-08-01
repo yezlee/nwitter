964fcb8b76817f92878f9c2ca68084b8b5dc303a
Yeseul Lee <dev.yezlee@gmail.com>
2021-08-01 17:10:32 +0900

    Making collection/document on firebase - add data - nweeting!

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -19,3 +20,5 @@ export const authService = firebase.auth();
 // const auth = fbase.auth(); // App.js에서 이렇게 해주거나 아님 fbase.js안에서 authService 변수를 만들어서 export해주거나
 // 여기다가 이렇게 하는 이유는, auth서비스를 많이 호출할 예정이어서.
 // 이렇게 하면 다른곳에서 authService를 이용할때마다 firebase.auth()를 호출해야하는데 여기서 호출해고 그걸 export해버리면 다른곳에서 import만 하면 되니까
+
+export const dbService = firebase.firestore();
